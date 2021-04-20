f69dce011d194c295b989b96cb59b58dd61b46db
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-04-20 11:44:39 +0530

    First commit, Set-up typescript

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+// character can have strings. 
+var character = 'mario';
+console.log(character);
+// Interaction with DOM using query selectors is possible.
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+// character can have strings. 
+const character = 'mario';
+
+console.log(character);
+
+// Interaction with DOM using query selectors is possible.
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+    console.log(input);
+})
+
